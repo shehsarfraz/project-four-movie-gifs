e6f193bab47be744d3e75d914cb3305069507bd7
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2023-05-24 10:14:05 -0400

    small change

--- src/components/GifSection.js
+++ src/components/GifSection.js
@@ -4,9 +4,19 @@ function GifSection() {
         <section className="gif-section">
             <div className="wrapper">
                 <div className="gif-group-container">
-                    <div className="gif-container"></div>
-                    <div className="gif-container"></div>
-                    <div className="gif-container"></div>
+                    <div className="gif-container">
+
+                        <img src="https://media1.giphy.com/media/3o7qDEq2bMbcbPRQ2c/200.gif"
+                        />
+                    </div>
+                    <div className="gif-container">
+                        <img src="https://media1.giphy.com/media/ZazzhhJxVUu5RLADVm/200.gif"
+                        />
+                    </div>
+                    <div className="gif-container">
+                        <img src="https://media1.giphy.com/media/lPXLrBtF4hYSxUfRhi/200.gif"
+                        />
+                    </div>
                 </div>
             </div>
         </section>
