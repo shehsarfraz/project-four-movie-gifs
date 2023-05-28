ac0ee2c0fe92936dcb603fb72d1c5f4c4ae59b8c
AMBERNAY TROTTER <AMBERNAYTROTTER@GMAIL.COM>
2023-05-28 12:17:13 -0400

    changed gifs from divs to list items

--- src/components/GifDisplay.js
+++ src/components/GifDisplay.js
@@ -1,14 +1,14 @@
 const GifDisplay = ({gifUrls}) => {
-    return ( 
+    return (
         gifUrls.map((individualGifData) => {
         return (
-            <div className="gif-container" key={individualGifData.id}>
+            <li className="gif-container" key={individualGifData.id}>
                 <a href={individualGifData.url}><img src={`https://media1.giphy.com/media/${individualGifData.id}/200.gif`} alt={individualGifData.title}/></a>
-            </div>
+            </li>
             )
         })
     )
-                
+
 }
 
 export default GifDisplay;
